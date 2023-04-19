79bdd04e23dd4af0a2253820b83063b247a244d4
Leonardo Florêncio <leonardo_florenciop@outlook.com>
2023-04-19 19:23:16 -0300

    Validação do formulário

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
-async function consultaEndereco () {
-    
-    let cep = document.querySelector('#campoDePesquisa').value;
-    
-    let url = `https://viacep.com.br/ws/${cep}/json/`;
-    
-    await fetch(url).then(async function(response){
-        await response.json().then(mostrarEndereco) 
-        })
-    }
-
-    function mostrarEndereco (dados) {
+function consultaCEP () {
+        let CEPDigitado = document.querySelector('#campoDePesquisa').value;
 
         let resultado = document.querySelector('#formularioDeResposta');
-        const caracteresCEP = document.getElementById('campoDePesquisa').value;
 
+        console.log(CEPDigitado.length);
 
-        
-        console.log(dados)
-
-        if (dados.erro) {
-            resultado.innerHTML = "O CEP fornecido não foi encontrado";
+        if (CEPDigitado.length < 8) {
+            resultado.innerHTML = "O CEP deve conter 8 dígitos"
         } else {
-            resultado.innerHTML = `<p><strong>Rua:</strong> ${dados.logradouro}</p>
-                                    <p><strong>Bairro:</strong> ${dados.bairro}</p>
-                                    <p><strong>Cidade:</strong> ${dados.localidade}</p>
-                                    <p><strong>Estado:</strong> ${dados.uf}</p>`
+            consultaEndereco()
         }
+
+        async function consultaEndereco () {
+    
+            const cep = document.querySelector('#campoDePesquisa').value;
+            
+            const url = `https://viacep.com.br/ws/${cep}/json/`;
+        
+            await fetch(url).then(async function(response){
+                await response.json().then(mostrarEndereco) 
+                })
+            }
+        
+            function mostrarEndereco (dados) {
+        
+                let resultado = document.querySelector('#formularioDeResposta');
+        
+                if (dados.erro) {
+                    resultado.innerHTML = "O CEP fornecido não foi encontrado";
+                } else {
+                    resultado.innerHTML = `<p><strong>Rua:</strong> ${dados.logradouro}</p>
+                                            <p><strong>Bairro:</strong> ${dados.bairro}</p>
+                                            <p><strong>Cidade:</strong> ${dados.localidade}</p>
+                                            <p><strong>Estado:</strong> ${dados.uf}</p>`
+                }
+            }
     }
-
-   
-
-
-
-
-
-
